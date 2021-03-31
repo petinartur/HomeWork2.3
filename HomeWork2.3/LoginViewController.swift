fe31fd57f6272792755924fc45a32524163e0356
Artur <petin.art@gmail.com>
2021-03-31 13:46:18 +0300

    first version

---
+++ HomeWork2.3/LoginViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  HomeWork2.3
+//
+//  Created by Артур Петин on 30.03.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVS = segue.destination as? WelcomeViewController else {return}
+        welcomeVS.userWelcome = user
+    }
+        let user = "Test"
+        let pass = "Qwerty1"
+    
+    @IBAction func forgetUserName(_ sender: Any) {
+        let alert = UIAlertController(title: "You're user name", message: "\(user)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+        NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    @IBAction func forgetPassword(_ sender: Any) {
+        let alert = UIAlertController(title: "You're password", message: "\(pass)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+        NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func loginButton(_ sender: Any) {
+        let enterUserName = (userName.text ?? "")
+        let enterPassword = (password.text ?? "")
+        if enterUserName != user || enterPassword != pass{
+            let alert = UIAlertController(title: "Ошибка", message: "Не верный логин или пароль", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+    }
+    
+    
+}
+
---
+++ HomeWork2.3/WelcomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WelcomeViewController.swift
+//  HomeWork2.3
+//
+//  Created by Артур Петин on 31.03.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var welcomeMessage: UILabel!
+    
+    var userWelcome = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeMessage.text = "Hi! \(userWelcome)"
+        }
+    
+
+
+    
+    @IBAction func logout(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+
+}
